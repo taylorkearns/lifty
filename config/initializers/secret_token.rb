67a8edd89d37a706d6eeae2a7e97c0957c0152cd
Taylor Kearns <tkearns@gmail.com>
2012-03-20 17:17:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifty::Application.config.secret_token = '64df41709f5c7f25ea705b24537f5e95efcb657347d04abec49befe66d971cdef5b0e4c8e6372e5fb3c84cd891a806645cfe23fae8f71091751bda47ef780f1a'
