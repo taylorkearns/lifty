b1fb3a557e38fcce5416309f17d39276d4d6bed7
Taylor Kearns <tkearns@gmail.com>
2012-03-31 14:35:09 -0400

    Routes and actions for Exercise, Sass organizing
    Wired up new, create, update, destroy actions for exercises. Using ajax
    on update. Need to create index page listing all exercises. No JS
    validation, Rails seems to be doing some of it, not sure how I want to
    handle. Renamed Sass files as includes.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,9 +1,46 @@
 class ExercisesController < ApplicationController
 	def index
-		@exercises = Exercise.all()
+		@exercises = Exercise.all
+	end
+	
+	def new
+		@exercise = Exercise.new
+	end
+	
+	def create
+		@exercise = Exercise.new(params[:exercise])
+		if @exercise.save
+			@exercise.name = params['exercise']['name']
+			@exercise.weight_each_side = params['exercise']['weight_each_side'].to_i
+			@exercise.bar_weight = params['exercise']['bar_weight'].to_i
+			@exercise.weight_units = params['exercise']['weight_units']
+			@exercise.reps = params['exercise']['reps'].to_i
+			flash[:notice] = "Exercise successfully created."
+			redirect_to exercise_path(@exercise)
+		else
+			flash[:notice] = "Unable to create exercise. Please make sure all fields are filled out."
+			render 'new'
+		end
 	end
 	
 	def show
 		@exercise = Exercise.find(params[:id])
+	end
+	
+	def update
+		@exercise = Exercise.find(params[:id])
+		@exercise.name = params['exercise']['name']
+		@exercise.weight_each_side = params['exercise']['weight_each_side'].to_i
+		@exercise.bar_weight = params['exercise']['bar_weight'].to_i
+		@exercise.weight_units = params['exercise']['weight_units']
+		@exercise.reps = params['exercise']['reps'].to_i
+		@exercise.save
+		render 'show'
+	end
+	
+	def destroy
+		@exercise = Exercise.find(params[:id])
+		@exercise.delete
+		redirect_to '/'
 	end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,8 +2,7 @@ class Exercise < ActiveRecord::Base
 	validates :name, :presence => true
 	validates :weight_each_side, :presence => true
 	validates :bar_weight, :presence => true
-	validates :total_weight, :presence => true
+	#validates :total_weight, :presence => true
 	validates :weight_units, :presence => true
-	validates :type_of_weight, :presence => true
 	validates :reps, :presence => true
 end
